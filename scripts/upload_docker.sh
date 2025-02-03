67ac2915b1beb13129ca974ec548e528cc71da2d
Sir AppSec <sirappsec@gmail.com>
2025-02-03 22:24:22 +0200

    docker: upload_docker.sh to support environment variables for username password
    necessary for the jenkins job

--- scripts/upload_docker.sh
+++ scripts/upload_docker.sh
@@ -4,11 +4,14 @@
 CONFIG_FILE="../config.yml"
 eval "$(sed -n 's/^docker:/docker_/p' $CONFIG_FILE)"
 
-# Build Docker image
-docker build -t $docker_image_name:$docker_image_tag ..
+# Build Docker image with arguments
+docker build \
+  --build-arg DOCKER_USERNAME=$DOCKER_USERNAME \
+  --build-arg DOCKER_PASSWORD=$DOCKER_PASSWORD \
+  -t $docker_image_name:$docker_image_tag ..
 
 # Log in to Docker registry
-echo "$docker_password" | docker login -u "$docker_username" --password-stdin $docker_registry_url
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin $docker_registry_url
 
 # Tag and push Docker image
 docker tag $docker_image_name:$docker_image_tag $docker_registry_url/$docker_image_name:$docker_image_tag
